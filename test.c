7e45ec0e14a5f47abf8fed2db845cce6913cf24c
Mikko Finell <mikko.finell@gmail.com>
2016-01-20 15:44:51 +0200

    Add testsuite meta test
    Call the ct_init, ct_report, and ct_terminate functions to check
    behavior.

---
+++ test.c
@@ -0,0 +1,16 @@
+#include "libctest.h"
+
+int main(void) {
+    ct_init("testsuit");
+
+    ct_report("pass test", 1);
+    ct_report("fail test", 0);
+    ct_report("pass test again", 1);
+    ct_report("fail again", 0);
+    ct_report("pass again", 1);
+    ct_report("fail more", 0);
+    ct_report("fail more", 0);
+    
+    ct_terminate();
+    return 0;
+}
