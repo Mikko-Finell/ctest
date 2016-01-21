e2e0d27071f3314068b5adf990d54dd978ce966b
Mikko Finell <mikko.finell@gmail.com>
2016-01-21 06:30:53 +0200

    Split statistics printing from memory cleanup
    Created ct_print_stats that outputs stats without freeing
    memory, and ct_print_stats_and_terminate which does both, along with
    ct_terminate which only frees memory.

--- libctest.h
+++ libctest.h
@@ -1,10 +1,14 @@
-#ifndef __testing__
-#define __testing__
+#ifndef __ctest__
+#define __ctest__
 
 extern void ct_init(const char * module_name);
 
 extern void ct_report(const char * test_name, int success);
 
+extern void ct_print_stats(void);
+
 extern void ct_terminate(void);
+
+extern void ct_print_stats_and_terminate(void);
 
 #endif
